42b85a58768f9614d535168242bb17f7aeb6f04c
MiguelMorassuti <miguelguimaraesmorassuti@hotmail.com>
2023-02-14 22:14:34 -0300

    - feat: validate token string strip

--- 1.py
+++ 1.py
@@ -6,33 +6,27 @@ hasReservedWord = [
     'int', 'long', 'register', 'return',
     'short', 'signed', 'sizeof', 'static',
     'struct', 'switch', 'typedef', 'union',
-    'unsigned',	'void',	'volatile',	'while'] #palavras reservadas
+    'unsigned',	'void',	'volatile',	'while'] #palavras reservadas 
 
 hasInteger = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] #números inteiros
-hasToken = ['+', '-', '*', '/', '%', '++', '--'] #operadores artmeticos, relacionas e caracteres especiais
+hasGeneral = ['+', '-', '*', '/', '%', '++', '--'] #operadores artmeticos, relacionas e caracteres especiais
 lineContent = []
-response = []
+lineTokens = []
 
 code = open('arquivoemc.txt', 'r')
 content = code.readlines()
 
 def compiler():
     for line in content:
-        response.clear
-        cleanContent = line.replace(' ', '')
-        lineContent.a
-        
-       
-        print(cleanContent)
-    return
+        cleanContent = line.split(' ')
+        for item in cleanContent:
+            if(hasReservedWord._contains_(item)):
+                print('TOKEN: Palavra reservada')
+            if(hasInteger._contains_(item)):
+                print('TOKEN: Número inteiro')
 
 compiler()
 
 '''
-        if():
-            response.__add__('Palavra reservada')
-        if():
-            response.__add__('Inteiro')
-        if():
-            response.__add__('Token')
+       
 '''
